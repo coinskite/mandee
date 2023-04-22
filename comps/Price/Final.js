9ae7f4f319a8b5626f9635b3596a491048b84247
Raj kumar <raj@coinskite.com>
2023-04-22 19:52:06 +0530

    map finished

--- comps/Price/Final.js
+++ comps/Price/Final.js
@@ -1,11 +1,11 @@
-import Tn from '../../svg/map/Tn.svg';
+import Tn from './Tn';
 
 function Final() {
   return (
     <div className="dfc items-center p-6 text-center">
       <p className="underline underline-offset-4 decoration-[#00AA21] decoration-2">Commodities prices by District wise</p>
       <p>Click on a district for a detailed price</p>
-      <Tn className="w-60 h-96" />
+      <Tn />
     </div>
   )
 }
